2f7738211dd1f1c8c96732cf22f0b716ec27852a
Fred Neumann <fred.neumann@fau.de>
2017-05-24 15:20:26 +0200

    fix for mantis 20604 - increased minimum ILIAS version

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $version = "1.0.0";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.0.0";
+$ilias_min_version = "5.0.18";
 $ilias_max_version = "5.2.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
