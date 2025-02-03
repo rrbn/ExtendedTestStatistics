cacfa13b74cb75e90a217044eddc5b78daeedae5
fneumann <fneumann@databay.de>
2025-02-03 08:35:43 +0100

    Fix 043843: Identifier "ilUser" is not defined

--- classes/class.ilExtendedTestStatisticsPlugin.php
+++ classes/class.ilExtendedTestStatisticsPlugin.php
@@ -1,4 +1,5 @@
 <?php
+
 // Copyright (c) 2017 Institut fuer Lern-Innovation, Friedrich-Alexander-Universitaet Erlangen-Nuernberg, GPLv3, see LICENSE
 
 
@@ -9,9 +10,8 @@
  */
 class ilExtendedTestStatisticsPlugin extends ilUserInterfaceHookPlugin
 {
-
-	protected ?ilExtendedTestStatisticsConfig $config;
-    protected ilObjUser $user;
+    protected ?ilExtendedTestStatisticsConfig $config;
+    protected ?ilObjUser $user;
 
     /**
      * Constructor
@@ -22,37 +22,41 @@ class ilExtendedTestStatisticsPlugin extends ilUserInterfaceHookPlugin
         string $id
     ) {
         global $DIC;
-        $this->user = $DIC->user();
+
+        // https://mantis.ilias.de/view.php?id=43843
+        if ($DIC->offsetExists("ilUser")) {
+            $this->user = $DIC->user();
+        }
 
         parent::__construct($db, $component_repository, $id);
     }
 
-	/**
-	 * After update processing
-	 */
+    /**
+     * After update processing
+     */
     protected function afterUpdate(): void
-	{
-		parent::afterUpdate();
-		ilExtendedTestStatisticsCache::flushAll();
-	}
+    {
+        parent::afterUpdate();
+        ilExtendedTestStatisticsCache::flushAll();
+    }
 
     public function getPluginName(): string
-	{
-		return "ExtendedTestStatistics";
-	}
+    {
+        return "ExtendedTestStatistics";
+    }
 
     public function refreshLanguages()
     {
         $this->getLanguageHandler()->updateLanguages();
     }
 
-	public function getConfig(): ilExtendedTestStatisticsConfig
-	{
-		if (!isset($this->config)) {
-			$this->config = new ilExtendedTestStatisticsConfig($this);
-		}
-		return $this->config;
-	}
+    public function getConfig(): ilExtendedTestStatisticsConfig
+    {
+        if (!isset($this->config)) {
+            $this->config = new ilExtendedTestStatisticsConfig($this);
+        }
+        return $this->config;
+    }
 
     protected function buildLanguageHandler(): ilPluginLanguage
     {
@@ -60,27 +64,32 @@ class ilExtendedTestStatisticsPlugin extends ilUserInterfaceHookPlugin
     }
 
     /**
-	 * Get debugging output of different value formats
-	 */
-	public function debugFormats(): bool
-	{
-		return false;
-	}
+     * Get debugging output of different value formats
+     */
+    public function debugFormats(): bool
+    {
+        return false;
+    }
 
-	/**
-	 * Get a user preference
-	 */
-	public function getUserPreference(string $name, string $default = ''): string
-	{
-        return $this->user->getPref($this->getId() . '_' . $name) ?? $default;
-	}
+    /**
+     * Get a user preference
+     */
+    public function getUserPreference(string $name, string $default = ''): string
+    {
+        if ($this->user !== null) {
+            return $this->user->getPref($this->getId() . '_' . $name) ?? $default;
+        }
+        return $default;
+    }
 
 
-	/**
-	 * Set a user preference
-	 */
-	public function setUserPreference(string $name, string $value) : void
-	{
-		$this->user->writePref($this->getId() . '_' . $name, $value);
-	}
+    /**
+     * Set a user preference
+     */
+    public function setUserPreference(string $name, string $value): void
+    {
+        if ($this->user !== null) {
+            $this->user->writePref($this->getId() . '_' . $name, $value);
+        }
+    }
 }
